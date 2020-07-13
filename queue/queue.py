0943f4ef7f1e2804bb53b82d3f2c95a8619d5835
JGPico <minq_2@hotmail.com>
2020-07-13 17:24:10 -0600

    Near MVP

--- queue/queue.py
+++ queue/queue.py
@@ -14,24 +14,37 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
-# from singly_linked_list import Node, LinkedList
+from singly_linked_list import Node, LinkedList
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        self.values = []
+        # self.values = []
+        self.values = LinkedList()
 
     def __len__(self):
-        return len(self.values)
+        # return len(self.values)
+        current = self.values.head
+
+        if current == None:
+            return 0
+        else:
+            while current.get_next() is not self.values.tail:
+                current = current.get_next()
+
+        return current
 
     def enqueue(self, value):
-        self.values.insert(0, value)
+        # self.values.insert(0, value)
+        self.values.add_to_tail(value)
 
     def dequeue(self):
-        if self.values == []:
+        # if self.values == []:
+        #     return None
+        # else:
+        #     return self.values.pop()
+        if self.values.head == None:
             return None
         else:
-            # deq = self.values[-1]
-            return self.values.pop()
-            # return deq
+            return self.values.remove_head()
--- stack/stack.py
+++ stack/stack.py
@@ -10,26 +10,43 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-# from singly_linked_list import Node, LinkedList
+from singly_linked_list import Node, LinkedList
 
 
 class Stack:
     def __init__(self):
         self.size = 0
-        self.values = []
+        # self.values = []
+        self.values = LinkedList()
 
     def __len__(self):
-        return len(self.values)
+        # return len(self.values)
+        current = self.values.head
+
+        if current == None:
+            return 0
+        else:
+            while current.get_next() is not self.values.tail:
+                current = current.get_next()
+
+        return current
 
     def push(self, value):
-        self.values.append(value)
+        # self.values.append(value)
+        self.values.add_to_tail(value)
 
     def pop(self):
-        if self.values == []:
+        # if self.values == []:
+        #     return None
+        # else:
+        #     return self.values.pop()
+        if self.values.head == None:
             return None
         else:
-            return self.values.pop()
+            return self.values.remove_tail()
 
     def peek(self):
-        if not self.values == []:
-            return self.values[-1]
+        # if not self.values == []:
+        #     return self.values[-1]
+        if not self.values.head == None:
+            return self.values.tail
